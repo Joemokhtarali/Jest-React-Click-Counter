8ef9e97e62be73a30dd5d1ebb3490ba1a3d07ee8
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2021-03-24 14:33:30 -0400

    refactoring by building helper functions for DRY code

--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -5,20 +5,24 @@ import EnzymeAdaptor from '@wojtekmaj/enzyme-adapter-react-17'
 
 Enzyme.configure( { adapter: new EnzymeAdaptor()})
 
+// helper functions to create a shallowWrapper for the app component
+const setup = () => shallow( <App />) 
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`)
+
 test('renders without error', () => {
-  const wrapper = shallow( <App />)
-  const appComponent = wrapper.find("[data-test='component-app']")
+  const wrapper = setup()
+  const appComponent = findByTestAttr(wrapper, "component-app")
   expect(appComponent.length).toBe(1)
 });
 
 test("renders button", () => {
-  const wrapper = shallow( <App />)
-  const button = wrapper.find("[data-test='increment-button']")
+  const wrapper = setup()
+  const button = findByTestAttr(wrapper, "increment-button")
   expect(button.length).toBe(1)
 })
 test("renders counter display", () => {
-  const wrapper = shallow( <App />)
-  const counterDisplay = wrapper.find("[data-test='counter-display']")
+  const wrapper = setup()
+  const counterDisplay = findByTestAttr(wrapper, "counter-display")
   expect(counterDisplay.length).toBe(1)
 })
 
