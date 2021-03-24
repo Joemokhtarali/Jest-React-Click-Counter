5b3ae2adefd6b88ff378687dc18f4aee44f5388a
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2021-03-24 14:24:40 -0400

    first test using wrapper & toBe method on button and display

--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -1,8 +1,30 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Enzyme, { shallow } from 'enzyme'
+import EnzymeAdaptor from '@wojtekmaj/enzyme-adapter-react-17'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure( { adapter: new EnzymeAdaptor()})
+
+test('renders without error', () => {
+  const wrapper = shallow( <App />)
+  const appComponent = wrapper.find("[data-test='component-app']")
+  expect(appComponent.length).toBe(1)
 });
+
+test("renders button", () => {
+  const wrapper = shallow( <App />)
+  const button = wrapper.find("[data-test='increment-button']")
+  expect(button.length).toBe(1)
+})
+test("renders counter display", () => {
+  const wrapper = shallow( <App />)
+  const counterDisplay = wrapper.find("[data-test='counter-display']")
+  expect(counterDisplay.length).toBe(1)
+})
+
+test("counter starts at 0", () => {
+
+})
+test("clickingon button increments counter display", () => {
+
+})
